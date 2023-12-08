c59bf8d3b44f37d59471ba8264819e0362efdcec
David Lee <allmywant@gmail.com>
2023-12-08 11:40:42 +0800

    Wrap the defensive check into a function

--- gulpfile.js
+++ gulpfile.js
@@ -60,6 +60,17 @@ function test()
     return runTests();
 }
 
+function insertPreventDuplicateLoadingCheck(fileName) {
+    let content = fs.readFileSync(fileName, 'utf8');
+    let position = content.indexOf('Mojito =');
+    if (position == -1) {
+        position = content.indexOf('Mojito=');
+    }
+
+    content = content.substr(0, position) + 'if (!window.Mojito || !Mojito.testObjects || !Object.keys(Mojito.testObjects).length) {' + content.substring(position) + '}';
+    fs.writeFileSync(fileName, content);
+}
+
 function build()
 {
     del(['dist/assets/js']);
@@ -93,6 +104,16 @@ function build()
                         'Mojito container built (', gzippedSize, ')');
                 });
                 
+                callback(null, file);
+            }))
+            .pipe(through.obj(function(file, enc, callback)
+            {
+                // add defensive check to prevent Mojito gets loading multi times
+                if (!config.allowMultiInstance) {
+                    insertPreventDuplicateLoadingCheck(`dist/assets/js/${containerName}.js`);
+                    insertPreventDuplicateLoadingCheck(`dist/assets/js/${containerName}.pretty.js`);
+                }
+
                 callback(null, file);
             }))
         );
@@ -169,23 +190,8 @@ function build()
         {
             // add defensive check to prevent Mojito gets loading multi times
             if (!config.allowMultiInstance) {
-                let content = fs.readFileSync(`dist/assets/js/${containerName}.js`, 'utf8');
-                let position = content.indexOf('Mojito =');
-                if (position == -1) {
-                    position = content.indexOf('Mojito=');
-                }
-
-                content = content.substr(0, position) + 'if (!window.Mojito || !Mojito.testObjects || !Object.keys(Mojito.testObjects).length) {' + content.substring(position) + '}';
-                fs.writeFileSync(`dist/assets/js/${containerName}.js`, content);
-
-                content = fs.readFileSync(`dist/assets/js/${containerName}.pretty.js`, 'utf8');
-                position = content.indexOf('Mojito =');
-                if (position == -1) {
-                    position = content.indexOf('Mojito=');
-                }
-
-                content = content.substr(0, position) + 'if (!window.Mojito || !Mojito.testObjects || !Object.keys(Mojito.testObjects).length) {' + content.substring(position) + '}';
-                fs.writeFileSync(`dist/assets/js/${containerName}.pretty.js`, content);
+                insertPreventDuplicateLoadingCheck(`dist/assets/js/${containerName}.js`);
+                insertPreventDuplicateLoadingCheck(`dist/assets/js/${containerName}.pretty.js`);
             }
 
             callback(null, file);
